5d9702580a43993c00882a49cf18ffb97ce297c1
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-04 14:03:41 -0800

    client

--- client.js
+++ client.js
@@ -1,13 +1,19 @@
+const net = require('net');
+
 const connect = function() {
-    const conn = net.createConnection({
-      host: "localhost",
-      port: 50541
-    });
-    // interpret incoming data as text
-    conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log("you ded cuz you idled");
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('conect', () => {
+    console.log('Name: NTM');
   })
-  
-    return conn;
-  }
+  conn.on('data', (data) => {
+    console.log(data);
+  })
+  return conn;
+}
+
+module.exports = {connect};
