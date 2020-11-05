04d9b43b04ad486fbb7f9b607fbd435cec5f2a3d
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-04 16:43:58 -0800

    input module

---
+++ input.js
@@ -0,0 +1,34 @@
+const handleUserInput = function(data) {
+    if (data === '\u0003') {
+      process.exit();
+    }
+    switch (data) {
+      case 'w':
+        connection.write('Move: up');
+        break;
+      case 's':
+        connection.write('Move: down');
+        break;
+      case 'a':
+        connection.write('Move: left');
+        break;
+      case 'd':
+        connection.write('Move: right');
+        break;
+    }
+  };
+  
+  const setupInput = function(connection) {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    stdin.on('data', (data) => {
+      handleUserInput(data);
+    });
+    return stdin;
+  }
+  
+  setupInput();
+
+  module.exports = {setupInput};
