f5c0af1a4cfb491062937f7b76e34e470cba8015
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-04 16:37:05 -0800

    stdin setup

--- play.js
+++ play.js
@@ -1,3 +1,42 @@
-const { connect } = require('./client');
-console.log('Connecting ...');
-connect();
+const {
+    connect
+  } = require('./client');
+  console.log('Connecting ...');
+  connect();
+  
+  const handleUserInput = function(data) {
+    if (data === '\u0003') {
+      process.exit();
+    }
+    switch (data) {
+      case 'w':
+        connection.write('Move: up');
+        break;
+      case 's':
+        connection.write('Move: down');
+        break;
+      case 'a':
+        connection.write('Move: left');
+        break;
+      case 'd':
+        connection.write('Move: right');
+        break;
+    }
+  };
+  
+  
+  
+  
+  const setupInput = function(connection) {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    stdin.on('data', (data) => {
+      handleUserInput(data);
+    });
+    return stdin;
+  }
+  
+  setupInput();
+  
